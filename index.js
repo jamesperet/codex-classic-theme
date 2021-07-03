40c74b0656b632e6e074f0cf511533a8a123d1e9
James Peret <james.peret@gmail.com>
2021-07-03 19:25:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+class ClassicTheme {
+    
+    constructor(program, config){
+        this.program = program;
+        this.config = config;
+        this.module_name = "Classic Theme";
+    }
+
+    init()
+    {
+        
+    }  
+    run(){}
+}
+
+module.exports = ClassicTheme;
